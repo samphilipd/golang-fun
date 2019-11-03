8ca546f1ce8a9e61b5305b7e67e5668745095c76
Sam Davies <sampdavies@gmail.com>
2019-11-03 08:09:07 +0000

    Is tree symmetric

--- binary_tree.go
+++ binary_tree.go
@@ -11,6 +11,12 @@ Otherwise, the NOT null node will be used as the node of new tree.
 
 package main
 
+import (
+	"fmt"
+
+	"container/list"
+)
+
 type TreeNode struct {
 	Val   int
 	Left  *TreeNode
@@ -68,4 +74,94 @@ func InvertTree(root *TreeNode) *TreeNode {
 	root.Right = right
 	root.Left = left
 	return root
+}
+
+/*
+101. Symmetric Tree
+Easy
+
+Given a binary tree, check whether it is a mirror of itself (ie, symmetric around its center).
+
+For example, this binary tree [1,2,2,3,4,4,3] is symmetric:
+
+    1
+   / \
+  2   2
+ / \ / \
+3  4 4  3
+
+
+
+But the following [1,2,2,null,3,null,3] is not:
+
+    1
+   / \
+  2   2
+   \   \
+   3    3
+
+
+
+Note:
+Bonus points if you could solve it both recursively and iteratively.
+*/
+// This is a Breadth-first search problem
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func IsSymmetricRecursive(root *TreeNode) bool {
+	return isMirror(root, root)
+}
+
+func isMirror(left *TreeNode, right *TreeNode) bool {
+	if left == nil && right == nil {
+		return true
+	} else if left == nil {
+		return false
+	} else if right == nil {
+		return false
+	} else {
+		fmt.Printf("left.val: %v\n", left.Val)
+		return left.Val == right.Val &&
+			isMirror(left.Left, right.Right) &&
+			isMirror(left.Right, right.Left)
+	}
+}
+
+func IsSymmetricIterative(root *TreeNode) bool {
+	// Use a queue
+	l := list.New()
+	l.PushFront(root)
+	l.PushFront(root)
+
+	for l.Len() != 0 {
+		n1 := dequeue(l)
+		n2 := dequeue(l)
+		if n1 == nil && n2 == nil {
+			continue
+		} else if n1 == nil || n2 == nil {
+			return false
+		} else if n1.Val != n2.Val {
+			return false
+		}
+		enqueue(l, n1.Left)
+		enqueue(l, n2.Right)
+		enqueue(l, n1.Right)
+		enqueue(l, n2.Left)
+	}
+	return true
+}
+
+func enqueue(l *list.List, n *TreeNode) *list.Element {
+	return l.PushFront(n)
+}
+func dequeue(l *list.List) *TreeNode {
+	el := l.Back()
+	l.Remove(el)
+	return el.Value.(*TreeNode)
 }
